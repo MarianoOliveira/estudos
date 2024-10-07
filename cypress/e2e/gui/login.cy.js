318803b5b333d073ad2afe77e62a6d690a21b37b
Mariano Oliveira <m.oliveira3000@hotmail.com>
2024-10-07 17:26:52 -0300

    removing bad files

--- cypress/e2e/gui/login.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Login', () => {
-    it('Login com sucesso', () => {
-        cy.navegarAoSite();
-        cy.logar('mariano@mailinator.com', 'teste123');
-        cy.validarLogin();
-    });
-});
